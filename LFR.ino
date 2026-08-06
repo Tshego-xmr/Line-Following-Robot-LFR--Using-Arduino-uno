f823a503b0c595b13a893bb17801974f1490ebc7
Tshegofatso <montshabatho125@gmail.com>
2026-08-06 15:04:23 +0200

    Update IR sensor pin assignments(Fixed left sensor to pwm pin)

--- LFR.ino
+++ LFR.ino
@@ -6,8 +6,8 @@
 
 //3 Pin declaration
 // -------- IR SENSOR PINS --------
-const int RIGHT_SENSOR = 2;
-const int LEFT_SENSOR  = 3;
+const int RIGHT_SENSOR = 3;
+const int LEFT_SENSOR  = 11;
 
 //L298N PINS
 // Left motor
